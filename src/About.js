bf58b3ae075002552fe372561f87252f40ff4c60
mimichen123 <73910678+mimichen123@users.noreply.github.com>
2024-05-09 10:51:03 -0700

    Add files via upload

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ function About() {
             <p><strong>Instructions: </strong>Enter your name and submit to have acccess to the game. The game has two different levels.</p>
             
             <p><strong>Easy Level:</strong> Helps you quickly understand basics of the game.</p>
-            <p><strong>Game Level:</strong> Once you know hte basics, time to start the real game!</p>
+            <p><strong>Game Level:</strong> Once you know the basics, time to start the real game!</p>
             
             
         </div>
